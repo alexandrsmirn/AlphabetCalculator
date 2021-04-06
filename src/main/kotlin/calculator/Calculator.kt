8d9aafa17da1e65a0f9b4238e3d661e8cb917343
Grismat <smirnalexandr@rambler.ru>
2021-04-06 23:09:52 +0300

    Calculator class extended

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -4,49 +4,43 @@ import java.util.*
 import calculator.letters.LetterList
 
 class Calculator(firstRow: String) { //TODO: мб сделать не в конструкторе а при первом вызове proceedRow
-    private val usedCharacters: BooleanArray
+    private val usedCharacters: BooleanArray = BooleanArray(26) { false }
     private var previousRow: String
-    private val letterOrder: LetterList
+    private val letterOrder: LetterList = LetterList()
 
     init {
-        usedCharacters = BooleanArray(26, {false})
         previousRow = firstRow
-        letterOrder = LetterList()
     }
 
+    /**Determines if the alphabetic order keeps after proceeding current row*/
     fun proceedRow(currentRow: String) : Boolean {
-        val letters = getFirstMismatchChar(previousRow, currentRow)
-        val previousLetter = letters.first
+        val (previousLetter, currentLetter) = getFirstMismatchChar(previousRow, currentRow)
 
-        val currentLetter = letters.second
+        when {
+            currentLetter == '_'  -> return false
+            previousLetter == '_' -> return true
+        }
+
         val currentLetterIndex = currentLetter.toInt() - 'a'.toInt()
-
-        //TODO: разобраться с символом ' '
-
-        if (!usedCharacters[currentLetterIndex]) {
+        return if (!usedCharacters[currentLetterIndex]) {
             usedCharacters[currentLetterIndex] = true
             letterOrder.addAfterLetter(previousLetter, currentLetter)
-            return true
-        }
-        else {
-            return letterOrder.checkLetter(previousLetter, currentLetter)
+            true
+        } else {
+            letterOrder.checkLetter(previousLetter, currentLetter)
         }
     }
 
-    /**returns index in str2 of first mismatch with str1*/
+    /**Returns pair of the first different characters in strings firstString and secondString*/
     private fun getFirstMismatchChar(firstString: String, secondString: String): Pair<Char, Char> {
         for (i in firstString.indices) {
-            //guaranteed that there will be a mismatch due to a given input format
-            if (secondString[i] != firstString[i]) {
+            if (i == secondString.length) {                  //if the secondString is a substring of the firstString
+                return Pair(firstString[i], ' ')             //TODO: сделать красивее
+            } else if (secondString[i] != firstString[i]) {
                 return Pair(firstString[i], secondString[i])
             }
         }
-
-        //if the firstString is shorter than the secondString
+        //if the firstString is a substring of the secondString
         return Pair(' ', secondString[firstString.length])
     }
 }
-
-//Alexbndr
-//Alex
-//Alexandr
